8526708783f733e36e52860c8f98a892f9adc1e4
Valentina Maraio <valentina.maraio89@gmail.com>
2022-01-28 19:25:07 +0100

    cool

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Ciao!")
+
+name = input("enter your name: ")
+print("Ciao " + name)
+
+age = input("enter your age: ")
+print(int(age))
+
+print("Create a program that will ask a user how old they are. Print out some text telling them how old they will be next year.")
+
